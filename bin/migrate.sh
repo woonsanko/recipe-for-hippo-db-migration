5d41eea2d3b0687bf056e03281a8ed39362db62c
Woonsan Ko <woonsanko@users.noreply.github.com>
2018-01-11 21:49:27 -0500

    Update migrate.sh

--- bin/migrate.sh
+++ bin/migrate.sh
@@ -1,2 +1,49 @@
 #!/bin/sh
+# -----------------------------------------------------------------------------
+# Start Script for Migrating Hippo CMS Database
+#
+# Environment Variable Prequisites
+#
+#   JAVA_HOME       Must point at your Java Development Kit installation.
+#
+#   JAVA_OPTS       (Optional) Java runtime options used when executed.
+#
+# -----------------------------------------------------------------------------
 
+JAVA_OPTS="-Xms512m -Xmx1024m"
+
+# resolve links - $0 may be a softlink
+PRG="$0"
+
+while [ -h "$PRG" ]; do
+  ls=`ls -ld "$PRG"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '.*/.*' > /dev/null; then
+    PRG="$link"
+  else
+    PRG=`dirname "$PRG"`/"$link"
+  fi
+done
+
+PRGDIR=`dirname "$PRG"`
+TOOL_HOME=`cd "$PRGDIR/.." ; pwd`
+
+_LIBPATH=
+if [ -d "$TOOL_HOME/lib" ]; then
+  for i in "$TOOL_HOME"/*.jar; do
+    _LIBPATH="$_LIBPATH":"$i"
+  done
+fi
+
+CLASSPATH="$CLASSPATH":"$_LIBPATH"
+
+echo "Using JAVA_HOME:       $JAVA_HOME"
+
+java $JAVA_OPTS $CATALINA_OPTS \
+  -classpath "$CLASSPATH" \
+  org.apache.jackrabbit.standalone.Main \
+  --backup \
+  --conf conf/source/repository.xml \
+  --repo data/source/repository \
+  --backup-conf conf/target/repository.xml \
+  --backup-repo data/target/repository
